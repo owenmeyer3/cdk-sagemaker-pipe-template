0f292352d0f773da32e58224fff218f5e7ffb414
owenmeyer3 <owenmeyer3@gmail.com>
2026-06-23 16:02:21 +0000

    Initial commit

---
+++ tests/unit/test_cdk_sagemaker_pipe_template_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from cdk_sagemaker_pipe_template.cdk_sagemaker_pipe_template_stack import CdkSagemakerPipeTemplateStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in cdk_sagemaker_pipe_template/cdk_sagemaker_pipe_template_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = CdkSagemakerPipeTemplateStack(app, "cdk-sagemaker-pipe-template")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
